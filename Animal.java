0ede44f6d71e5c7f9009b9e2b702f481be2284fb
OliverQdy <47838166+OliverQdy@users.noreply.github.com>
2019-05-22 14:21:55 +0800

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,9 @@
+package to_Github;
+
+public class Animal {      //Father class
+      public int legNum;  //definition for status
+      
+      public void bark() { //definition for method
+    	  System.out.println("barking!");
+      }
+}
